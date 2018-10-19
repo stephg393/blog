7dcd5e8893df35b9438c7d5724e17bfa10d7d3c6
stephg393 <stephanie.gnammakou@gmail.com>
2018-10-19 11:26:25 -0400

    initial commit

---
+++ src/components/Bio.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import profilePic from './profile-pic.jpg'
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          fontFamily: 'Courier New, Courier, monospace',
+          display: 'flex',
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <img
+          src={profilePic}
+          alt={`Stephanie Gnammakou`}
+          style={{
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            width: rhythm(5),
+            height: rhythm(5),
+            borderRadius: 70,
+          }}
+        />
+        <p>
+          Hi, I'm <strong>Stephanie Gnammakou</strong>! I'm a student at at
+          University of Maryland, College Park doubling in computer science and
+          communcation.{' '}
+          <a href="https://www.linkedin.com/in/stephaniegnammakou/">
+            Check out my LinkedIn profile
+          </a>
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Bio
